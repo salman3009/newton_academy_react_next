18d752961cd7d4cadc6c93aea27c86fe293aa87a
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-01-11 16:01:16 +0530

    Code

---
+++ 5_phase_Nextjs/page-routing-assignment/11tooltip/components/TooltipPortal.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+
+const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
+
+function TooltipPortal({ children }) {
+  if (!portalRoot) return null;
+
+  return ReactDOM.createPortal(
+    children,
+    portalRoot
+  );
+}
+
+export default TooltipPortal;
