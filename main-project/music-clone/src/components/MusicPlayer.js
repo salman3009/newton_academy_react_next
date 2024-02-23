d2a915225af0edf67a27eb73746f7f730a86c28d
salman3009 <salman444g@gmail.com>
2024-02-23 10:21:45 +0530

    code

--- main-project/music-clone/src/components/MusicPlayer.js
+++ main-project/music-clone/src/components/MusicPlayer.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
+import { useUser } from '../helpers/UserProvider';
 
 export const MusicPlayer = (selectedMusic) => {
 
   const audioRef = useRef();
   const [addedToWatchlist, setAddedToWatchlist] = useState(false);
-
+  const {getUser} = useUser();
   const { thumbnail, title, artist, audio_url } = selectedMusic;
   const artistList = artist && artist.map((item) => item.name).join(" & ");
 
-  useEffect(()=>{
+  useEffect(() => {
     audioRef.current.play();
-  },[selectedMusic])
+  }, [selectedMusic])
+
+  const AddFavorite = (songId) => {
+    axios.patch('https://academics.newtonschool.co/api/v1/music/favorites/like', { "songId": songId }, {
+      headers: {
+        Authorization: `Bearer ${getUser.token}`
+      }
+    }).then((response) => {
+      setAddedToWatchlist(true);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
 
   return (
     <section className="music-player">
@@ -23,9 +36,9 @@ export const MusicPlayer = (selectedMusic) => {
           </div>
         </div>
         <audio controls ref={audioRef} src={audio_url} />
-        
+
         <div className="heart-icon">
-          {addedToWatchlist ? <i class="fa-solid fa-heart"></i> : <i class="fa-regular fa-heart"></i>}
+          {addedToWatchlist ? <i onClick={() => AddFavorite()} class="fa-solid fa-heart"></i> : <i class="fa-regular fa-heart"></i>}
         </div>
       </>
     </section>
--- main-project/music-clone/src/pages/Library.js
+++ main-project/music-clone/src/pages/Library.js
@@ -1,8 +1,80 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import MusicCard from '../components/MusicCard';
+import MusicPlayer from '../components/MusicPlayer';
+import Sidebar from '../pages/Sidebar';
+import { useUser } from '../helpers/UserProvider';
+
 function Library() {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [musicsList, setMusicList] = useState([]);
+  const [selectedMusic, setMusic] = useState(null);
+  const {getUser} = useUser();
+
+  const fetchMusics = async () => {
+    try {
+      setIsLoading(true);
+      const musics = await axios.get('https://academics.newtonschool.co/api/v1/music/favorites/like',{headers:{
+        Authorization:`Bearer ${getUser.token}`
+       }})
+      console.log("musics", musics.data.data);
+      const musicListData = musics.data.data.songs;
+      setMusicList(musicListData);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMusics();
+  }, []);
+
+  const setMusicHandler = (id) => {
+    let selectedMusic = musicsList[id];
+    setMusic(selectedMusic);
+  }
+
+  const removeFavorite=(songId)=>{
+    axios.patch('https://academics.newtonschool.co/api/v1/music/favorites/like',{ "songId" : songId},{headers:{
+      Authorization:`Bearer ${getUser.token}`
+     }}).then((response)=>{
+              console.log(response);
+              alert("successfully removed");
+              fetchMusics();
+     }).catch((error)=>{
+                console.log(error);
+     })
+  }
+
   return (<>
-    <section className="musicList-container">
-      <h1>Library</h1>
-    </section>
+    <div className="global-container">
+      <div className="left-bar">
+        <Sidebar />
+      </div>
+      <div className="right-bar">
+        <section className="musicList-container">
+          {musicsList.map((music, i) => (
+            <MusicCard key={i}
+              title={music.title}
+              thumbnail={music.thumbnail}
+              artist={music.artist}
+              id={i}
+              setSelectedMusic={setMusicHandler}
+            />
+          ))}
+        </section>
+      </div>
+    </div>
+    {selectedMusic && <section>
+      <MusicPlayer thumbnail={selectedMusic.thumbnail}
+        title={selectedMusic.title}
+        artist={selectedMusic.artist}
+        audio_url={selectedMusic.audio_url}
+      />
+    </section>}
   </>)
 }
 
